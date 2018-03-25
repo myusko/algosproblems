1e2956a7f6115f57184ddaed8e12f632cd71a14b
MichaelYusko <mykhaylo.yusko@gmail.com>
2018-03-25 18:39:14 +0300

    Add find_one_missing_number problem

---
+++ tests/test_arrays/find_one_number.py
@@ -0,0 +1,11 @@
+# Given an list containing 9 numbers ranging from 1 to 10,
+# write a function to find the missing number.
+# Assume you have 9 numbers between 1 to 10 and only one number is missing.
+
+
+def find_missing_number(list_numbers):
+    n = len(list_numbers)
+    total = (n + 1) * (n + 2) / 2
+    print(total)
+    total_sum = sum(list_numbers)
+    return total - total_sum
